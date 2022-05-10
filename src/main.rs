5f2c811748b0bdc5f23cb808132d59f63657c756
vee <emmanuele9090@gmail.com>
2022-05-10 15:32:40 +0000

    created the magic bytes func etc

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::io::prelude::*;
 use std::env;
 use std::fs;
 use std::fs::File;
+use rand::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,8 +12,25 @@ fn main() {
     let filename = &args[1];
     let fltwrt = get_bytes(filename.to_string());
     create_data(fltwrt);
+    create_magic();
 }
 
+fn create_magic() {
+    let magic_bytes: Vec<u32> =
+        vec![0xFF,
+             0x7F,
+             0x00,
+             0xFFFF,
+             0x0000,
+             0xFFFFFFFF,
+             0x00000000,
+             0x80000000,
+             0x40000000,
+             0x7FFFFFFF];
+    let testvalue: u8 = rand::random::<u8>() % 11;
+    println!("{}", testvalue)
+
+}
 fn create_data(data: Vec<u8>)  {
     let mut buffer = File::create("mutated.pcap").expect("error creating mutated.pcap");
     buffer.write_all(&data).expect("Error writing to mutated");
